0284a8483a28ef0047d75f81cbf2227437d08ef6
davidrayfr <davidrayfr@gmail.com>
2022-10-03 17:51:43 +0200

    Version with interruption

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,8 @@
 // Bouton sur GPIO 12
 // Allumage diode blanche
 // Check 1 clic -> On allume la Led Blanche
-// Check 2 clic
-// Check Long Clic
+// Check 2 clic -> clignotement Led Blanche
+// Check Long Clic -Clignotement Led Rouge et Blanche
 
 #include <Arduino.h>
 #include "OneButton.h"
@@ -14,48 +14,89 @@
 #define BUTTON_PIN 12 // Bouton Branché sur GPIO 12
 #define TIME_LONG_CLICK_DETECTION 5000 // Detection Tps Mini long clic in Millisecondes
 #define TIME_LONG_CLICK_START 1000 // Detection start Long Click
+#define TIME_BLINK 1000 // Time - Frequency blink for Led in MilliSecond
 
 // Setup a new OneButton on pin PIN_INPUT
 // The 2. parameter activeLOW is true, because external wiring sets the button to LOW when pressed.
 OneButton button(BUTTON_PIN, true,true);
+hw_timer_t *My_timer=NULL;
 
 // current LED state, staring with LOW (0)
 int whiteLedState = LOW;
 int redLedState = LOW;
+int longClickId = false;
 unsigned long pressStartTime;
 
-#if defined(ESP32)
+// Declaration indavance
+void whiteLedChange();
+void redLedChange();
+
 void IRAM_ATTR checkTicks() {
   // include all buttons here to be checked
   button.tick(); // just call tick() to check the state.
 }
 
-#endif
+void IRAM_ATTR onTimer() {
+  // include all buttons here to be checked
+  whiteLedChange();
+  if (longClickId)
+    redLedChange();
+}
+
+void whiteLedChange()
+{
+    digitalWrite(WHITE_LED_PIN, !digitalRead(WHITE_LED_PIN));
+}
+
+void redLedChange()
+{
+    digitalWrite(RED_LED_PIN, !digitalRead(RED_LED_PIN));
+}
+
+void whiteLedBlink()
+{
+    digitalWrite(WHITE_LED_PIN, !digitalRead(WHITE_LED_PIN));
+}
+
+void redLedBlink()
+{
+    digitalWrite(RED_LED_PIN, !digitalRead(RED_LED_PIN));
+}
+
 
 void doubleClick()
 {
-Serial.println("Double Click detected");
-
+Serial.println("Double Click detected > Clignotement LED BLANCHE");
+// Blink launch
+timerAlarmEnable(My_timer);
 }
 
 void simpleClick()
 {
 Serial.println("Simple Click detected");
-
+if (timerAlarmEnabled(My_timer)) {
+  timerAlarmDisable(My_timer);
+  longClickId=false;
+  }
+whiteLedChange();
 }
 
 void longClick()
 {
 Serial.println("Long Click");
-
+longClickId=true;
+timerAlarmEnable(My_timer);
 }
 
 // this function will be called when the button was held down for 1 second or more.
 void pressStart() {
   Serial.println("pressStart()");
+  if (timerAlarmEnabled(My_timer)) {
+    timerAlarmDisable(My_timer);
+    longClickId=false;
+  }
   pressStartTime = millis() - TIME_LONG_CLICK_START; // as set in setPressTicks()
 } // pressStart()
-
 
 // this function will be called when the button was released after a long hold.
 void pressStop() {
@@ -65,6 +106,7 @@ void pressStop() {
   if ((millis() - pressStartTime)>TIME_LONG_CLICK_DETECTION)
     {
       Serial.println("long hold detected");
+      longClick();
     }
   } // pressStop()
 
@@ -87,11 +129,16 @@ void setup()
     button.setPressTicks(TIME_LONG_CLICK_START); // that is the time when LongPressStart is called
     button.attachLongPressStart(pressStart);
     button.attachLongPressStop(pressStop);
+
+    // initialisation Od timer interrupt
+    My_timer=timerBegin(0,80,true);
+    timerAttachInterrupt(My_timer,&onTimer,true);
+    timerAlarmWrite(My_timer,TIME_BLINK*1000,true);
+
 }
 
 void loop()
 {
- 
     button.tick();
     delay(10);
 }
